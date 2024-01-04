984da92c7a379660e2ce0e64c03d24f77ef70e6c
Troll-Lol0059 <akashshaw0059@gmail.com>
2024-01-04 17:14:54 +0530

    StudyNotion Project - Commit No. 19 🚀🚀🚀 -> Completed Structure of Course Builder Part 1

--- StudyNotion Project/src/components/core/dashboard/addCourse/CourseInformationForm .jsx
+++ StudyNotion Project/src/components/core/dashboard/addCourse/CourseInformationForm .jsx
@@ -1,136 +1,188 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { useDispatch, useSelector } from 'react-redux';
-import {fetchCourseCategories} from '../../../../services/operations/courseDetailsAPI';
-import {HiOutlineCurrencyRupee} from 'react-icons/hi';
-import  RequirementsField from './RequirementsField';
+import { fetchCourseCategories } from '../../../../services/operations/courseDetailsAPI';
+import { HiOutlineCurrencyRupee } from 'react-icons/hi';
+import RequirementsField from './RequirementsField';
+import ChipInput from './ChipInput';
+import Thumbnail from './Thumbnail';
+import { MdNavigateNext } from "react-icons/md";
+import IconBtn from "../../../common/IconBtn";
+import { setCourse, setStep } from "../../../../slices/courseSlice";
 
 
-function CourseInformationForm () {
+
+function CourseInformationForm() {
   const {
     register,
     handleSubmit,
     setValue,
-    getValue,
-    formState:{errors}
-  } = useForm();
+    getValues,
+    formState: { errors },
+  } = useForm()
 
   const dispatch = useDispatch();
 
-  const {course,editCourse} = useSelector( (state) => state.course );
-  const [loading,setLoading] = useState(false);
-  const [courseCategories,setCourseCategories] = useState([]) ;
+  const { course, editCourse } = useSelector((state) => state.course);
+  const [loading, setLoading] = useState(false);
+  const [courseCategories, setCourseCategories] = useState([]);
 
-  useEffect( () => {
-    const getCategories = async() => {
+  useEffect(() => {
+    const getCategories = async () => {
       setLoading(true);
       const categories = await fetchCourseCategories();
       console.log(categories);
-      if(categories.length > 0){
+      if (categories.length > 0) {
         setCourseCategories(categories);
       }
       setLoading(false);
     }
 
-    if(editCourse){
-      setValue("courseTitle",course.courseName);
-      setValue("courseShortDesc",course.courseDescription);
-      setValue("coursePrice",course.price);
-      setValue("courseTags",course.tag);
-      setValue("courseBenefits",course.whatYouWillLearn);
-      setValue("courseCategory",course.category);
-      setValue("courseRequirements",course.instructions);      
-      setValue("courseImage",course.thumbnail);      
+    if (editCourse) {
+      setValue("courseTitle", course.courseName);
+      setValue("courseShortDesc", course.courseDescription);
+      setValue("coursePrice", course.price);
+      setValue("courseTags", course.tag);
+      setValue("courseBenefits", course.whatYouWillLearn);
+      setValue("courseCategory", course.category);
+      setValue("courseRequirements", course.instructions);
+      setValue("courseImage", course.thumbnail);
     }
 
     getCategories();
-  },[] )
+  }, [])
 
   const onSubmit = (data) => {
     console.log(data);
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='text-white rounded-md border-richblack-700 bg-richblack-800 space-y-8'>
-        <div>
-          <label htmlFor='courseTitle'>Course Title <sup>*</sup></label>
-          <input id='courseTitle'
-          placeholder='Enter Course Title' className='form-style'
-          {...register('courseTitle',{required:true})} 
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
+      {/* div for course title */}
+      <div className="flex flex-col space-y-2">
+        <label htmlFor='courseTitle' className="text-sm text-richblack-5">Course Title 
+        <sup className="text-pink-200">*</sup></label>
+        <input id='courseTitle'
+          placeholder='Enter Course Title' className="form-style w-full"
+          {...register('courseTitle', { required: true })}
+        />
+        {errors.courseTitle && (<span className='form-error-span'>Course title is required</span>)}
+      </div>
+
+      {/* Course Short Description */}
+      <div className="flex flex-col space-y-2">
+        <label htmlFor='courseShortDesc' className="text-sm text-richblack-5">Short Description 
+        <sup className="text-pink-200">*</sup> </label>
+        <textarea id='courseShortDesc'
+          placeholder='Enter Short Description' className='form-style resize-x-none min-h-[130px] w-full'
+          {...register('courseShortDesc', { required: true })}
+        />
+        {errors.courseShortDesc && (<span className='form-error-span'>Short Description is required</span>)}
+      </div>
+
+      <div className="relative flex flex-col space-y-2">
+        <label htmlFor='coursePrice' className="text-sm text-richblack-5">Course Price
+        <sup className="text-pink-200">*</sup></label>
+        <input
+            id="coursePrice"
+            placeholder="Enter Course Price"
+            {...register("coursePrice", {
+              required: true,
+              valueAsNumber: true,
+              pattern: {
+                value: /^(0|[1-9]\d*)(\.\d+)?$/,
+              },
+            })}
+            className="form-style w-full !pl-12"
           />
-          {errors.courseTitle && (<span className='form-error-span'>Course title is required</span>)}
-        </div>
 
-        <div>
-          <label htmlFor='courseShortDesc'>Short Description <sup>*</sup> </label>
-          <textarea id='courseShortDesc'
-          placeholder='Enter Short Description' className='form-style'
-          {...register('courseShortDesc',{required:true})} 
-          />
-          {errors.courseShortDesc && (<span className='form-error-span'>Short Description is required</span>)}
-        </div>
+        <HiOutlineCurrencyRupee className="absolute top-[58%] left-[2%] inline-block -translate-y-1/2 text-2xl text-richblack-400"  />
 
-        <div className='relative'>
-          <label htmlFor='coursePrice'>Course Price<sup>*</sup></label>
-          <input id='coursePrice'
-          placeholder='Enter Course Price' className='form-style'
-          {...register('coursePrice',
-          {required:true,
-            valueAsNumber:true,
-          })} 
-          />
+        {errors.coursePrice && (<span className='form-error-span'>Price is required</span>)}
+      </div>
 
-          <div className='absolute'>
-            <HiOutlineCurrencyRupee />
-          </div>
+      <div className="flex flex-col space-y-2">
+        <label htmlFor='courseCategory' className='text-sm text-richblack-5'>Course Category 
+        <sup className="text-pink-200">*</sup></label>
 
-          {errors.coursePrice && (<span className='form-error-span'>Price is required</span>)}
-        </div>
+        <select id='courseCategory' defaultValue={""}
+          className='form-style w-full'
+          {...register('courseCategory', { required: true })}
+        >
+ 
+          <option value="" disabled>Choose a Category</option>
 
-        <div>
-          <label htmlFor='courseCategory'>Course Category <sup>*</sup></label>
+          {
+            !loading && courseCategories.map((category, index) => (
+              <option key={index} value={category._id}>
+                {category?.name}
+              </option>
+            ))
+          }
 
-          <select id='courseCategory' defaultValue={""}
-          className='form-style'
-          {...register('courseCategory',{required:true})}
-          >
+        </select>
+        {errors.courseCategory && (<span className='form-error-span'>Course Category is required</span>)}
 
-              <option value="" disabled>Choose a Category</option>
+        {/* todo : add a tag component & thumbnail component*/}
 
-              {
-                !loading && courseCategories.map( (category,index) => (
-                  <option key={index} value={category._id}>
-                    {category?.name}
-                  </option>
-                ) )
-              }
+        <ChipInput label="Tags"
+        name="courseTags"
+        placeholder="Enter Tags and press Enter"
+        register={register}
+        errors={errors}
+        setValue={setValue}
+        getValues={getValues}
+        />
 
-          </select>
-          {errors.courseCategory &&(<span className='form-error-span'>Course Category is required</span>)}
-        
-          {/* todo : add a tag component & thumbnail component*/}
+        <Thumbnail name="courseImage"
+          label="Course Thumbnail"
+          register={register}
+          setValue={setValue}
+          errors={errors}
+          editData={editCourse ? course?.thumbnail : null}
+        />
 
-          <div>
-            <label htmlFor='courseBenefits'>Benefits of the Course <sup>*</sup></label>
-            <textarea id='courseBenefits'
+        <div className='flex flex-col gap-2'>
+          <label htmlFor='courseBenefits' className="text-sm text-richblack-5">Benefits of the Course 
+          <sup className="text-pink-200">*</sup></label>
+          <textarea id='courseBenefits'
             placeholder='Enter the course Benefits'
-            {...register('courseBenefits',{required:true})}
+            {...register('courseBenefits', { required: true })}
             className='form-style'
-            />
+          />
 
-            {errors.courseBenefits && (<span className='form-error-span'>Please Enter course Benefits</span>) }
-          </div>
+          {errors.courseBenefits && (<span className='form-error-span'>Please Enter course Benefits</span>)}
+        </div>
 
-          <RequirementsField 
+        <RequirementsField
           name='courseRequirements'
           label='Requirements/Instructions'
           register={register}
           errors={errors}
           setValue={setValue}
-          getValue={getValue}
-          />
+          getValues={getValues}
+        />
 
+        {/* Next Button */}
+        <div className="flex justify-end gap-x-2">
+          {editCourse && (
+            <button
+              onClick={() => dispatch(setStep(2))}
+              disabled={loading}
+              className={`flex cursor-pointer items-center gap-x-2 rounded-md bg-richblack-300 py-[8px] px-[20px] font-semibold text-richblack-900`}
+            >
+              Continue Wihout Saving
+            </button>
+          )}
+          <IconBtn
+            disabled={loading}
+            text={!editCourse ? "Next" : "Save Changes"}
+          >
+            <MdNavigateNext />
+          </IconBtn>
         </div>
+
+      </div>
 
     </form>
   )
--- StudyNotion Project/src/components/core/dashboard/addCourse/RenderSteps.jsx
+++ StudyNotion Project/src/components/core/dashboard/addCourse/RenderSteps.jsx
@@ -4,7 +4,7 @@ import { FaCheck } from 'react-icons/fa';
 import CourseInformationForm from './CourseInformationForm ';
 
 function RenderSteps() {
-    const { step } = useSelector((state) => state.course)
+    const { step } = useSelector((state) => state.course);
 
     const steps = [
         {
@@ -20,48 +20,68 @@ function RenderSteps() {
             title: "Course Publish",
         }
     ]
+
     return (
         <>
-            <div className='text-white'>
-                {
-                    steps.map((item, index) => (
-                        <div key={index}>
-                            <div className={`${step === item.id ? "bg-yellow-900 border-yellow-50 text-yellow-50" :
-                                "border-richblack-700 bg-richblack-800 text-richblack-300"}`}>
-                                {
-                                    step > item.id ? (<FaCheck />) : (item.id)
-                                }
-                            </div>
-                                {/* add code for dashes b/w label */}
-                         </div>
-                    ))
-                }
+            <div className="relative mb-2 flex w-full justify-center">
+                {steps.map((item) => (
+                    <>
+                        <div
+                            className="flex flex-col items-center "
+                            key={item.id}
+                        >
+                            <button
+                                className={`grid cursor-default aspect-square w-[34px] place-items-center rounded-full border-[1px] ${step === item.id
+                                        ? "border-yellow-50 bg-yellow-900 text-yellow-50"
+                                        : "border-richblack-700 bg-richblack-800 text-richblack-300"
+                                    } ${step > item.id && "bg-yellow-50 text-yellow-50"}} `}
+                            >
+                                {step > item.id ? (
+                                    <FaCheck className="font-bold text-richblack-900" />
+                                ) : (
+                                    item.id
+                                )}
+                            </button>
 
-
-                <div>
-                    {
-                        steps.map((item, index) => (
-                                <div key={index}>
-                                    <p>{item.title}</p>
-                                </div>
-
-                        ))
-                    }
-                </div>
-
-                {
-                    step === 1 && (<CourseInformationForm />)
-                }
-                {/* {
-                        step === 2 && ( <CourseBuilderForm /> )
-                    }
-                    {
-                        step === 1 && ( <PublishCourse /> )
-                    } */}
-
+                        </div>
+                        {item.id !== steps.length && (
+                            <>
+                                <div
+                                    className={`h-[calc(34px/2)] w-[33%]  border-dashed border-b-2 ${step > item.id ? "border-yellow-50" : "border-richblack-500"
+                                        } `}
+                                ></div>
+                            </>
+                        )}
+                    </>
+                ))}
             </div>
+
+            <div className="relative mb-16 flex w-full select-none justify-between px-4">
+                {steps.map((item) => (
+                    <>
+                        <div
+                            className="flex min-w-[130px] flex-col items-center gap-y-2"
+                            key={item.id}
+                        >
+
+                            <p
+                                className={`text-sm ${step >= item.id ? "text-richblack-5" : "text-richblack-500"
+                                    }`}
+                            >
+                                {item.title}
+                            </p>
+
+                        </div>
+
+                    </>
+                ))}
+            </div>
+            {/* Render specific component based on current step */}
+            {step === 1 && <CourseInformationForm />}
+            {/* {step === 2 && <CourseBuilderForm />} */}
+            {/* {step === 3 && <PublishCourse />} */}
         </>
     )
 }
 
-export default RenderSteps
+export default RenderSteps;
